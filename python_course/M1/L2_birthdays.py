11306f95e74e484c72fccd8a584fc37199d11afe
ajay27bhat <ajay27bhat@gmail.com>
2026-07-11 02:51:26 +0530

    Add L2_birthdays.py for 'Birthdays' activity

---
+++ python_course/M1/L2_birthdays.py
@@ -0,0 +1,13 @@
+# Store birthday dates in different variables
+alice_birthday = "15-01-2002"
+bob_birthday = "23-04-2001"
+charlie_birthday = "10-08-2003"
+david_birthday = "05-12-2000"
+emma_birthday = "18-09-1999"
+
+# Print the birthday dates
+print("Alice's Birthday :", alice_birthday)
+print("Bob's Birthday   :", bob_birthday)
+print("Charlie's Birthday :", charlie_birthday)
+print("David's Birthday :", david_birthday)
+print("Emma's Birthday  :", emma_birthday)
