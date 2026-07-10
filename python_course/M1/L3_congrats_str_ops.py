ed2034caf35de706c6021630c14ba23cb7068097
ajay27bhat <ajay27bhat@gmail.com>
2026-07-11 03:05:49 +0530

    Add L3_congrats_str_ops.py for 'Congratulations Message' activity

---
+++ python_course/M1/L3_congrats_str_ops.py
@@ -0,0 +1,11 @@
+# Program to convert a word from lowercase to uppercase
+# using string operations
+
+# Get input from the user
+word = input("Enter the word: ")
+
+# Convert the word to uppercase using the upper() string method
+uppercase_word = word.upper()
+
+# Display the result
+print("Word in uppercase:", uppercase_word)
