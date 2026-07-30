6229d967ba3357b647f71bbd49e2f38c328973a3
ajay27bhat <ajay27bhat@gmail.com>
2026-07-30 11:26:47 +0530

    Add L2_rev_order.py for 'Reverse order' activity in M2

---
+++ python_course/M2/L2_rev_order.py
@@ -0,0 +1,9 @@
+# input number greater than 1
+n = int(input("Enter the value of n: "))
+
+# print the numbers from n to 1
+print(f"numbers from {n} to 1 are:")
+
+# loop to print numbers
+for i in range(n, 0, -1):
+    print(i)
