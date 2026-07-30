77d7bdaa35432509f96c0f1095f1eba74a268199
ajay27bhat <ajay27bhat@gmail.com>
2026-07-30 12:07:16 +0530

    Add L6_polygon_using_turtle.py for 'Polygon' activity in M2

---
+++ python_course/M2/L6_polygon_using_turtle.py
@@ -0,0 +1,14 @@
+import turtle
+
+t = turtle.Turtle()
+
+sides = int(input("Enter the number of sides: "))
+length = int(input("Enter the length of each side: "))
+
+angle = 360 / sides
+
+for i in range(sides):
+    t.forward(length)
+    t.left(angle)
+
+turtle.done()
