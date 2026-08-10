35f66204729c0e9237ac257041f7055f41e185ed
ajay27bhat <ajay27bhat@gmail.com>
2026-08-10 15:39:33 +0530

    Add L2 other activities in M3 in 'Python Course'

---
+++ python_course/M3/L2_args/others/sum_whole.py
@@ -0,0 +1,10 @@
+def sum_numbers(a, b):
+    return a + b
+
+# Main program
+num1 = int(input("Enter first whole number: "))
+num2 = int(input("Enter second whole number: "))
+
+result = sum_numbers(num1, num2)
+
+print("Sum =", result)
