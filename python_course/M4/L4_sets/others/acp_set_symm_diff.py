541866c283474c1fff1bddf0ffd3da0cd4fcf2c5
ajay27bhat <ajay27bhat@gmail.com>
2026-08-10 16:39:04 +0530

    Add L4 other activities in M4 in 'Python Course'

---
+++ python_course/M4/L4_sets/others/acp_set_symm_diff.py
@@ -0,0 +1,9 @@
+set1 = {1, 2, 3, 4}
+set2 = {3, 4, 5, 6}
+
+# Find symmetric difference
+result = set1.symmetric_difference(set2)
+
+print("Set 1:", set1)
+print("Set 2:", set2)
+print("Symmetric Difference:", result)
